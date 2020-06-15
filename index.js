b963eec3a341aef5936370feea878e437794798d
sara altaee <sara.korea96@gmail.com>
2020-06-15 00:17:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // Add your code here
 function submitData(userName,email){
  let body=document.querySelector('body');
+ return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: email
+    })
+  })
+  .then((response) => response.json())
+  .then((object) => {
+    document.body.innerHTML = object['id']
+  })
+  .catch((error) => {
+    document.body.innerHTML = error.message
+  })
 }
