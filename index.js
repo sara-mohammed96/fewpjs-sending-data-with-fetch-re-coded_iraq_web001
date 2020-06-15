3ac7c186e441c627455a3d5512d4d5375d9495d1
sara altaee <sara.korea96@gmail.com>
2020-06-15 00:26:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName,email){
+ let body=document.querySelector('body');
+let info = {
+  name:userName,
+  email:email
+};
+
+let configurationObj = {
+
+  method : 'POST',
+  headers : { 'content-type':'application/json' , 'Accept':'application/json'},
+  body : JSON.stringify(info)
+
+}
+return fetch ('http://localhost:3000/users',configurationObj)
+.then(res=> res.json())
+.then(res => {
+
+  body.innerHTML = res.id;
+}).catch( error=> {
+
+body.innerHTML = error.message;
+})
+}
